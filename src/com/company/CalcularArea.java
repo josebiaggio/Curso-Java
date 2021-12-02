29df08132e5a1a6e6d10409fb8f648b3fb616438
josebiaggio <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>
2021-12-01 21:42:58 -0300

    Create class CalcularArea

---
+++ src/com/company/CalcularArea.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class CalcularArea {
+    public int quadrado(int x, int y) {
+        return x * y;
+    }
+
+    public int triangulo(int b, int h) {
+        return (b * h) / 2;
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,17 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        CalcularArea calcularArea = new CalcularArea();
 
+        // Lados do quadrado: a e b
+        int x = 10, y = x;
+        int areaDoQuadrado = calcularArea.quadrado(x, y);
+        System.out.printf("A área do quadrado é %dm2\n", areaDoQuadrado);
+
+        // Base(b) e altura(h) de um triângulo
+        int b = 12, h = 8;
+
+        int areaDoTriangulo = calcularArea.triangulo(b, h);
+        System.out.printf("A área do triângulo é %dm2", areaDoTriangulo);
     }
 }

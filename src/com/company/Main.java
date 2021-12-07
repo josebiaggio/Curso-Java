ebd271146ee62c18402ca8cfae097b46ce9a85b9
josebiaggio <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>
2021-12-06 21:43:48 -0300

    First commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,22 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.printf("Informe o turno em que você estuda M[MATUTINO], V[VESPERTINO] ou N[NOTURNO]: ");
+        String turno = scanner.next();
 
+        if(turno.equals("M")) {
+            System.out.println("Bom dia!");
+        } else if(turno.equals("V")) {
+            System.out.println("Boa tarde!");
+        } else if(turno.equals("N")) {
+            System.out.println("Boa noite!");
+        } else {
+            System.out.println("Valor inválido");
+        }
     }
 }
